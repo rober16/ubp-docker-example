26fafe0869d88a39a3c11919f26cd7feec5f46c7
kaajavi <javierguignard@gmail.com>
2020-04-30 08:29:52 -0300

    Add TP

--- app/app.py
+++ app/app.py
@@ -1,15 +1,19 @@
 from flask import Flask
-import mysql.connector
-import json
 from flask import jsonify
-
+import mysql.connector
+import os
 app = Flask(__name__)
 
 
 def get_connection():
+    """
+    This function connect with db on http://db host (dockerized)
+    Needs DB_USER_NAME and DB_PASSWORD
+    :return: DB connection
+    """
     config = {
-        'user': 'root',
-        'password': 'root',
+        'user': os.environ.get('DB_USER_NAME','root'),
+        'password': os.environ.get('DB_PASSWORD','root'),
         'host': 'db',
         'port': '3306',
         'database': 'drinks'
@@ -19,8 +23,16 @@ def get_connection():
 
 @app.route('/')
 def index():
-    return u'HOLASSS!!!!!!'
-
+    """
+    Web's homepage
+    :return: HTTP response
+    """
+    hello = os.environ.get('NAME_HELLO',None)
+    if not hello:
+        hello='Tu variable de entorno "NAME_HELLO" no se cargo correctamente'
+    else:
+        hello = 'Hola {}'.format(hello)
+    return u'{}'.format(hello)
 
 @app.route('/wines/')
 def wines():
@@ -31,7 +43,6 @@ def wines():
     cursor.close()
     connection.close()
     return jsonify({'wines':wines})
-    # return json.dumps({'wines':wines})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
